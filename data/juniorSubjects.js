efdd7c0e098bda8067a96190e8f52eb47b39518e
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-04 10:12:52 -0700

    first Commit

--- data/juniorSubjects.js
+++ data/juniorSubjects.js
@@ -9,15 +9,15 @@ const juniorSubjects = [
   { id: 6, name: 'Cultural and Creative Arts' },
   { id: 7, name: 'Physical and Health Education' },
   { id: 8, name: 'Christian Religious Studies' },
-  { id: 7, name: 'Computer Studies' },
-  { id: 8, name: 'Home Economics' },
-  { id: 9, name: 'Hausa' },
-  { id: 10, name: 'Basic Technology' },
-  { id: 11, name: 'Agricultral Science' },
-  { id: 12, name: 'Music'},
-  { id: 13, name: 'French'},
-  { id: 14, name: 'History'},
-  { id: 15, name: 'Buisiness Studies'}
+  { id: 9, name: 'Computer Studies' },
+  { id: 10, name: 'Home Economics' },
+  { id: 11, name: 'Hausa' },
+  { id: 12, name: 'Basic Technology' },
+  { id: 13, name: 'Agricultral Science' },
+  { id: 14, name: 'Music'},
+  { id: 15, name: 'French'},
+  { id: 16, name: 'History'},
+  { id: 17, name: 'Buisiness Studies'}
 ];
 
 module.exports = juniorSubjects;
