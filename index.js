da9fd0f8729aa62d8fcbc9a2b9efa854bc44c836
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-05 02:54:17 -0700

    Changes

--- index.js
+++ index.js
@@ -4,36 +4,39 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
+const MongoStore = require('connect-mongo');
+const path = require('path');
 const authRoutes = require('./routes/authRoutes');
 const lessonPlanRoutes = require('./routes/lessonPlanRoutes');
-const path = require('path');
-const sessionRoutes = require('./routes/sessionRoutes')
-
+const sessionRoutes = require('./routes/sessionRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(cors());
-app.use(bodyParser.json());
-
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, './uploads'))); // Ensure correct path
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
+// Session setup
 app.use(session({
   secret: process.env.SESSION_SECRET || 'someRandomSessionSecret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase', // Use environment variable
+  }),
+  cookie: { secure: process.env.NODE_ENV === 'production', maxAge: 60000 }, // Secure cookies in production
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./auth/passport'); 
+require('./auth/passport'); // Ensure passport configuration is correct
 
-mongoose.connect('mongodb://127.0.0.1:27017/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase', {
   serverSelectionTimeoutMS: 5000,
   socketTimeoutMS: 45000
 })
@@ -43,9 +46,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mydatabase', {
   process.exit(1);
 });
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/lesson-plans', lessonPlanRoutes);
-app.use('/sessions', sessionRoutes); // Ensure this path matches client-side request
-
+app.use('/sessions', sessionRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
