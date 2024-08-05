99a9cee09f724d2593bff8babb52a3c567fdc0be
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-05 02:36:29 -0700

    Changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session'); // Correct import for express-session
 const app = express();
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // Middleware setup for session
 app.use(session({
