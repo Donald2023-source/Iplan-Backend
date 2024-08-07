4b4465ae1cfb62db34548950dde0d7e45816bea6
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-07 03:45:36 -0700

    I made changes

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(bodyParser.json());
 
