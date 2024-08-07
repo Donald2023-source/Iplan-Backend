b7da53bc0315fd02b75fd127ea2c47de19eb4525
Donald Yusuf <donalddyusuf@gmail.com>
2024-08-07 02:55:50 -0700

    Changes

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(bodyParser.json({ limit: '50mb' }));
